acaa2fb598756034dac7bfb9cade68c0be8dd515
JJJJ <penolove15@gmail.com>
2016-10-07 17:40:39 +0800

    add DEFAULT

--- example1/README.md
+++ example1/README.md
@@ -2,6 +2,8 @@ simple example in oslo.config
 
 that reads variables defined in app.conf
 
+for the [DEFAULT] variables in app.conf needn't group while register
+
 useage :
 ```
 python app.py
--- example1/app.py
+++ example1/app.py
@@ -10,11 +10,17 @@ simple_opts = [
                 help=('True enables, False disables'))
 ]
  
+loser_opts = [
+    cfg.BoolOpt('loser', default=False,
+                help=('True enables, False disables'))
+]
 CONF = cfg.CONF
 CONF.register_group(opt_group)
 CONF.register_opts(simple_opts, opt_group)
+CONF.register_opts(loser_opts)
  
  
 if __name__ == "__main__":
     CONF(default_config_files=['app.conf'])
     print(CONF.simple.enable)
+    print(CONF.loser)
